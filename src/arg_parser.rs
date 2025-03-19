3a648f4b94bb068be8a3a82ea0d907f8dce32d86
Přemysl Bednárek <xbednar6@fi.muni.cz>
2025-03-19 08:02:34 +0100

    Enhance argument parsing to support stdin input and return inputs alongside command

--- src/arg_parser.rs
+++ src/arg_parser.rs
@@ -1,3 +1,5 @@
+use std::io::{stdin, BufRead, BufReader};
+
 use crate::{
     structs::{
         derive_key_input::DeriveKeyInput, key_expression_input::KeyExpressionInput,
@@ -14,24 +16,59 @@ pub enum Command {
     ScriptExpression(ScriptExpressionInput),
 }
 
-pub fn parse_args(args: Vec<&str>) -> Result<Command, ParsingError> {
-    // if args includes --help, we should print the help message
-    if args.contains(&"--help") {
-        return Ok(Command::Help);
+pub type Inputs = Box<dyn Iterator<Item = String>>;
+
+/// Get the inputs for the sub-command
+/// The inputs are read from stdin if the '-' argument is present in args
+/// Otherwise, the argument right after the sub-command is the input
+/// Only the argument immediately following the sub-command is used as input.
+/// Additional flags or arguments (e.g., --foo) are not considered.
+fn get_inputs(args: &Vec<&str>) -> Result<Inputs, ParsingError> {
+    // if '-' is present in args, we should read from stdin
+    if args.contains(&"-") {
+        return Ok(Box::new(BufReader::new(stdin()).lines().map(|line| {
+            line.unwrap_or_else(|e| {
+                eprintln!("Error reading from stdin: {}", e);
+                std::process::exit(1);
+            })
+        })));
     }
 
-    // if --help is not present, then exacly one of the three sub-commands must be present and must be the first one
-    match args.first() {
-        Some(arg) => match *arg {
-            "derive-key" => Ok(Command::DeriveKey(DeriveKeyInput::parse(args)?)),
-            "key-expression" => Ok(Command::KeyExpression(KeyExpressionInput::parse(args)?)),
-            "script-expression" => Ok(Command::ScriptExpression(ScriptExpressionInput::parse(
-                args,
-            )?)),
-            _ => Err(ParsingError::new(&format!("Invalid argument: {}", arg))),
-        },
-        None => Err(ParsingError::new("No valid argument was provided.")),
+    // otherwise, the argument right after the sub-command is the input
+    let input = args
+        .get(1)
+        .ok_or_else(|| ParsingError::new("No input for the sub-command provided."))?;
+
+    Ok(Box::new(vec![input.to_string()].into_iter()))
+}
+
+/// Parse the given arguments into a command and inputs.
+pub fn parse_args(args: Vec<&str>) -> Result<(Command, Inputs), ParsingError> {
+    // if args includes --help, we should print the help message
+    if args.contains(&"--help") {
+        return Ok((Command::Help, Box::new(std::iter::empty::<String>())));
     }
+
+    // if --help is not present, then exacly one of the three sub-commands must be present and must be the first one argument
+    let first_arg = args.first().ok_or_else(|| {
+        ParsingError::new("No argument provided. Please specify the sub-command.")
+    })?;
+
+    let inputs = get_inputs(&args)?;
+
+    let command = match *first_arg {
+        "derive-key" => Command::DeriveKey(DeriveKeyInput::parse(args)?),
+        "key-expression" => Command::KeyExpression(KeyExpressionInput::parse(args)?),
+        "script-expression" => Command::ScriptExpression(ScriptExpressionInput::parse(args)?),
+        _ => {
+            return Err(ParsingError::new(&format!(
+                "Invalid argument: {}",
+                first_arg
+            )))
+        }
+    };
+
+    Ok((command, inputs))
 }
 
 mod tests {
@@ -51,29 +88,41 @@ mod tests {
         ];
 
         for arg in help_command_args.iter() {
-            assert!(matches!(parse_args(arg.to_vec()), Ok(Command::Help)));
+            assert!(matches!(parse_args(arg.to_vec()), Ok((Command::Help, _))));
         }
     }
 
     #[test]
     fn test_parse_args() {
         assert!(matches!(
-            parse_args(vec!["key-expression"]),
-            Ok(Command::KeyExpression(_))
+            parse_args(vec!["key-expression", "arg1"]),
+            Ok((Command::KeyExpression(_), _))
         ));
 
         assert!(matches!(
-            parse_args(vec!["script-expression"]),
-            Ok(Command::ScriptExpression(_))
+            parse_args(vec!["script-expression", "arg2"]),
+            Ok((Command::ScriptExpression(_), _))
         ));
 
         assert!(matches!(
-            parse_args(vec!["derive-key"]),
-            Ok(Command::DeriveKey(_))
+            parse_args(vec!["derive-key", "arg3"]),
+            Ok((Command::DeriveKey(_), _))
         ));
 
         assert!(parse_args(vec!["invalid"]).is_err());
 
         assert!(parse_args(vec![]).is_err());
+    }
+
+    #[test]
+    fn test_inputs() {
+        let inputs = get_inputs(&vec!["key-expression", "input"]).unwrap();
+        assert_eq!(inputs.collect::<Vec<String>>(), vec!["input"]);
+
+        assert!(get_inputs(&vec!["key-expression", "-"]).is_ok());
+
+        assert!(get_inputs(&vec!["key-expression"]).is_err());
+
+        assert!(get_inputs(&vec!["key-expression", "input1", "input2"]).is_ok());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+
+use arg_parser::Command;
 mod arg_parser;
 mod structs;
 mod traits;
@@ -10,9 +12,29 @@ fn main() {
     // convert the Vec<String> to Vec<&str>
     let args: Vec<&str> = args.iter().map(|s| s.as_str()).collect();
 
-    #[allow(unused_variables)]
-    let command = arg_parser::parse_args(args).unwrap_or_else(|err| {
+    let (command, inputs) = arg_parser::parse_args(args).unwrap_or_else(|err| {
         eprintln!("{}", err);
         std::process::exit(1);
     });
+
+    if command == Command::Help {
+        println!("Help message");
+        std::process::exit(0);
+    }
+
+    for input in inputs {
+        println!("{}", input);
+        match command {
+            Command::KeyExpression(_) => {
+                println!("Running Key expression subcommand with input: {}", input);
+            }
+            Command::ScriptExpression(_) => {
+                println!("Running Script expression subcommand with input: {}", input);
+            }
+            Command::DeriveKey(_) => {
+                println!("Running Derive key subcommand with input: {}", input);
+            }
+            Command::Help => { /* Already handled */ }
+        }
+    }
 }
