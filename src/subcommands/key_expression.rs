ef8819c4275ccacdcfedf7acc03cbec306fce060
Marek Skácelík <xskacel@fi.muni.cz>
2025-03-30 15:03:46 +0200

    Returning String when asked executing subcommand successfully (part of Result), bit of refactoring of the main.rs

--- src/subcommands/key_expression.rs
+++ src/subcommands/key_expression.rs
@@ -1,5 +1,8 @@
 use crate::structs::{key_expression_config::KeyExpressionConfig, parsing_error::ParsingError};
 
-pub fn key_expression(_input: String, _config: &KeyExpressionConfig) -> Result<(), ParsingError> {
-    Ok(())
+pub fn key_expression(
+    _input: String,
+    _config: &KeyExpressionConfig,
+) -> Result<String, ParsingError> {
+    todo!();
 }
