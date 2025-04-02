5348962def1c0ce837e46a27b8d88316e97588c4
Přemysl Bednárek <xbednar6@fi.muni.cz>
2025-04-02 23:43:17 +0200

    Add extended key validation utility

---
+++ src/subcommands/utils/extended_key.rs
@@ -0,0 +1,51 @@
+use crate::structs::parsing_error::ParsingError;
+use bip32::{ChildNumber, XPrv, XPub};
+use std::str::FromStr;
+
+/// Validate whether key is xpub encoded extended public key or xprv encoded extended private key (as defined in BIP 32):
+///     Followed by zero or more /NUM or /NUMh path elements indicating BIP 32 derivation steps to be taken after the given extended key.
+///     Optionally followed by a single /* or /*h final step to denote all direct unhardened or hardened children.
+pub fn validate_extended_key(key: &str) -> Result<(), ParsingError> {
+    if !(key.starts_with("xpub") || key.starts_with("xprv")) {
+        return Err(ParsingError::new("Key must start with xpub or xprv"));
+    }
+
+    let (key, path) = key.split_at(key.find('/').unwrap_or(key.len()));
+
+    // Check if the key is valid
+    if key.starts_with("xpub") {
+        XPub::from_str(key).map_err(|e| ParsingError::new(&format!("Invalid xpub key: {}", e)))?;
+    } else {
+        XPrv::from_str(key).map_err(|e| ParsingError::new(&format!("Invalid xprv key: {}", e)))?;
+    }
+
+    if path.is_empty() {
+        return Ok(());
+    }
+
+    let mut derivation_segments: Vec<&str> = path[1..].split('/').collect();
+
+    if derivation_segments.last() == Some(&"*") || derivation_segments.last() == Some(&"*h") {
+        derivation_segments.pop();
+    }
+
+    for segment in derivation_segments {
+        ChildNumber::from_str(segment).map_err(|e| {
+            ParsingError::new(&format!("Invalid derivation segment '{}': {}", segment, e))
+        })?;
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_validate_extended_key_valid() {
+        let input = "xprvA1RpRA33e1JQ7ifknakTFpgNXPmW2YvmhqLQYMmrj4xJXXWYpDPS3xz7iAxn8L39njGVyuoseXzU6rcxFLJ8HFsTjSyQbLYnMpCqE2VbFWc/3h/4h/5h/*h";
+        let result = validate_extended_key(input);
+        assert!(result.is_ok());
+    }
+}
--- src/subcommands/utils/mod.rs
+++ src/subcommands/utils/mod.rs
@@ -1,3 +1,4 @@
 pub mod checksum;
+pub mod extended_key;
 pub mod hex_encoded_public_key;
 pub mod hexadecimal;
